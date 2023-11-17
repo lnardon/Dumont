c6f8d8a7e29c9b4f7cf57b8b3aa3f2f23946a65e
lnardon <lucaslnardon@gmail.com>
2023-11-17 03:21:05 -0300

    ✨ Add delete container

--- frontend/react/src/App.tsx
+++ frontend/react/src/App.tsx
@@ -7,11 +7,34 @@ function App() {
 
   function sendRepoLink() {
     const containerPort = prompt("Enter the port you want to use:");
-    console.log(containerPort);
     fetch("/cloneRepo", {
       method: "POST",
-      body: JSON.stringify({ repo_url: repoLink }),
-    }).then((parsed) => console.log(parsed));
+      body: JSON.stringify({
+        repo_url: repoLink,
+        container_port: containerPort,
+      }),
+    }).then((res) => {
+      if (res.status === 201) {
+        alert("Project cloned and started");
+      } else {
+        alert("Error cloning and stating project");
+      }
+      window.location.reload();
+    });
+  }
+
+  function handleDelete(containerId: string) {
+    fetch("/deleteContainer", {
+      method: "POST",
+      body: JSON.stringify({ container_id: containerId }),
+    }).then((res) => {
+      if (res.status === 200) {
+        alert("Container deleted");
+      } else {
+        alert("Error deleting container");
+      }
+      window.location.reload();
+    });
   }
 
   useEffect(() => {
@@ -20,7 +43,6 @@ function App() {
     }).then((parsed) => {
       parsed.json().then((data) => {
         setContainerList(data);
-        console.log(data);
       });
     });
   }, []);
@@ -37,13 +59,15 @@ function App() {
       <button onClick={sendRepoLink}>Deploy</button>
 
       <div>
-        {containerList.map((container: any) => {
+        {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
+        {containerList?.map((container: any) => {
           return (
             <div>
               <h3>{container.Names}</h3>
               <p>{container.Status}</p>
               <p>{container.Ports}</p>
               <p>{container.CreatedAt}</p>
+              <button onClick={() => handleDelete(container.ID)}>Delete</button>
             </div>
           );
         })}
