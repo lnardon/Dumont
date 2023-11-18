7a06df2cf3aa90dad7d2b2c7f24de7df55d39449
lnardon <lucaslnardon@gmail.com>
2023-11-18 20:33:00 -0300

    ⚰️ Remove old vanilla project

--- frontend/main.js
+++
@@ -1,4 +0,0 @@
-function sendRepoLink(){
-    const url = document.getElementById("repoLink").value
-    fetch("/cloneRepo", {method: "POST", body: JSON.stringify({repo_url: url })}).then(parsed => console.log(parsed))
-}
